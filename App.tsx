cb0a3745a55ad72bc6fced169bd2cb6a60ffea40
skoll-vald <skollvald@gmail.com>
2023-08-30 18:05:09 +0300

    feat: add swipeble project delete, add project bg color changing on complete

--- App.tsx
+++ App.tsx
@@ -3,22 +3,25 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import GoogleSignIn from './Auth';
 import {View, Text} from 'react-native';
-import ProjIn from './ProjIn'; // Import your ProjectDetailsScreen component
+import ProjIn from './ProjIn'; // Import Project Details Screen component
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Projects">
-        <Stack.Screen name="Projects" component={GoogleSignIn} />
-        <Stack.Screen
-          name="ProjIn"
-          component={ProjIn}
-          options={{title: 'Project Overview'}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Projects">
+          <Stack.Screen name="Projects" component={GoogleSignIn} />
+          <Stack.Screen
+            name="ProjIn"
+            component={ProjIn}
+            options={{title: 'Project Overview'}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
 
--- ProjList.tsx
+++ ProjList.tsx
@@ -1,15 +1,10 @@
 import React, {useEffect, useState} from 'react';
-import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  ScrollView,
-} from 'react-native';
+import {View, Text, TextInput, ScrollView} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {RootStackParamList} from './NavigationTypes';
 import firestore from '@react-native-firebase/firestore'; // Import Firebase Firestore module
 import auth from '@react-native-firebase/auth';
+import {Swipeable, TouchableOpacity} from 'react-native-gesture-handler';
 
 interface Task {
   id: string;
@@ -92,27 +87,93 @@ const ProjList: React.FC = () => {
     navigation.navigate('ProjIn', {projectText});
   };
 
+  const deleteTask = async (taskId: string) => {
+    try {
+      const currentUser = auth().currentUser;
+      if (currentUser) {
+        await firestore()
+          .collection('users')
+          .doc(currentUser.uid)
+          .collection('tasks')
+          .doc(taskId)
+          .delete();
+        fetchTasks(); // Fetch the updated tasks for the user
+      }
+    } catch (error) {
+      console.error('Error deleting task:', error);
+    }
+  };
+
+  const renderSwipeableRow = (task: Task) => {
+    const renderRightActions = () => (
+      <TouchableOpacity
+        style={{
+          backgroundColor: 'red',
+          justifyContent: 'center',
+          alignItems: 'flex-start',
+          height: '100%',
+          padding: 10,
+        }}
+        onPress={() => deleteTask(task.id)}>
+        <Text style={{color: 'white'}}>Delete</Text>
+      </TouchableOpacity>
+    );
+
+    const renderLeftActions = () => (
+      <TouchableOpacity
+        style={{
+          backgroundColor: 'green',
+          justifyContent: 'center',
+          alignItems: 'flex-end',
+          height: '100%',
+          padding: 10,
+        }}
+        onPress={() => console.log('Left action')}>
+        <Text style={{color: 'white'}}>Complete</Text>
+      </TouchableOpacity>
+    );
+
+    return (
+      <Swipeable
+        renderRightActions={renderRightActions}
+        renderLeftActions={renderLeftActions}
+        overshootRight={false}
+        overshootLeft={false}>
+        <View
+          style={{
+            backgroundColor: task.completed ? 'gray' : 'white',
+            flexDirection: 'row',
+            alignItems: 'center',
+            paddingTop: 10,
+            padding: 10,
+          }}>
+          <TouchableOpacity
+            onPress={() => toggleCompleted(task.id, !task.completed)}>
+            <Text>{task.completed ? '☑' : '☐'}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              paddingLeft: 10,
+            }}
+            onPress={() => navigateToProjIn(task.text)}>
+            <Text
+              style={{
+                textDecorationLine: task.completed ? 'line-through' : 'none',
+              }}>
+              {task.text}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </Swipeable>
+    );
+  };
+
   return (
     <View style={{flex: 1, padding: 20}}>
       <ScrollView>
-        {tasks.map((task, index) => (
-          <View
-            key={task.id}
-            style={{flexDirection: 'row', alignItems: 'center', marginTop: 10}}>
-            <TouchableOpacity
-              onPress={() => toggleCompleted(task.id, !task.completed)}>
-              <Text>{task.completed ? '☑' : '☐'}</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={{flex: 1}}>
-              <Text
-                style={{
-                  textDecorationLine: task.completed ? 'line-through' : 'none',
-                }}
-                onPress={() => navigateToProjIn(task.text)}>
-                {task.text}
-              </Text>
-            </TouchableOpacity>
-          </View>
+        {tasks.map(task => (
+          <View key={task.id}>{renderSwipeableRow(task)}</View>
         ))}
       </ScrollView>
       <View
