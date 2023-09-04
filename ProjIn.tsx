d8d1d48da03aa3f46587122ca31349d24924da0a
skoll-vald <skollvald@gmail.com>
2023-09-04 13:25:45 +0300

    feat: added description field and firestoring it

--- ProjIn.tsx
+++ ProjIn.tsx
@@ -18,8 +18,32 @@ const ProjIn: React.FC<ProjInScreenProps> = ({route}) => {
   const [updatedText, setUpdatedText] = useState(projectText); // State to hold the updated text
   const [isDatePickerVisible, setDatePickerVisible] = useState(false);
   const [deadline, setDeadline] = useState(null); // State to hold the deadline date and time
+  const [description, setDescription] = useState('');
 
   // Fetch the deadline from Firestore when the component mounts
+  useEffect(() => {
+    const fetchDescription = async () => {
+      try {
+        const currentUser = auth().currentUser;
+        if (currentUser) {
+          const doc = await firestore()
+            .collection('users')
+            .doc(currentUser.uid)
+            .collection('tasks')
+            .doc(taskId)
+            .get();
+          const taskData = doc.data();
+          if (taskData && taskData.description) {
+            setDescription(taskData.description);
+          }
+        }
+      } catch (error) {
+        console.error('Error fetching description:', error);
+      }
+    };
+    fetchDescription();
+  }, [taskId]);
+
   useEffect(() => {
     const fetchDeadline = async () => {
       try {
@@ -72,7 +96,7 @@ const ProjIn: React.FC<ProjInScreenProps> = ({route}) => {
     setDatePickerVisible(false);
   };
 
-  const handleConfirm = async date => {
+  const updateDlInFirestore = async date => {
     setDeadline(date); // Update the deadline state with the selected date
     hideDatePicker();
     try {
@@ -92,14 +116,41 @@ const ProjIn: React.FC<ProjInScreenProps> = ({route}) => {
     }
   };
 
+  const updateDescriptionInFirestore = async () => {
+    try {
+      const currentUser = auth().currentUser;
+      if (currentUser) {
+        await firestore()
+          .collection('users')
+          .doc(currentUser.uid)
+          .collection('tasks')
+          .doc(taskId)
+          .update({
+            description: description,
+          });
+      }
+    } catch (error) {
+      console.error('Error updating task description:', error);
+    }
+  };
+
   return (
     <View style={{flex: 1, padding: 20}}>
+      <Text
+        style={{
+          color: 'gray',
+          fontSize: 12,
+        }}>
+        project name
+      </Text>
       <TextInput
         style={{
           color: 'black',
+          fontWeight: 'bold',
           borderBottomWidth: 1,
+          borderColor: 'lightgray',
           marginBottom: 20,
-          fontSize: 16,
+          fontSize: 20,
           padding: 5,
         }}
         value={updatedText}
@@ -107,12 +158,23 @@ const ProjIn: React.FC<ProjInScreenProps> = ({route}) => {
         onSubmitEditing={updateTextInFirestore}
       />
       <Text
-        style={{color: 'blue'}}
+        style={{
+          color: 'gray',
+          fontSize: 12,
+          marginTop: 40,
+        }}>
+        deadline
+      </Text>
+      <Text
+        style={{
+          color: 'green',
+          fontSize: 16,
+        }}
         onPress={() => {
           showDatePicker();
         }}>
         {deadline
-          ? `Deadline: ${new Date(deadline).toLocaleDateString()} ${new Date(
+          ? `${new Date(deadline).toLocaleDateString()} ${new Date(
               deadline,
             ).toLocaleTimeString()}`
           : 'Add deadline'}
@@ -120,8 +182,32 @@ const ProjIn: React.FC<ProjInScreenProps> = ({route}) => {
       <DateTimePickerModal
         isVisible={isDatePickerVisible}
         mode="datetime"
-        onConfirm={handleConfirm}
+        onConfirm={updateDlInFirestore}
         onCancel={hideDatePicker}
+      />
+      <Text
+        style={{
+          color: 'gray',
+          fontSize: 12,
+          marginTop: 40,
+        }}>
+        description
+      </Text>
+      <TextInput
+        style={{
+          color: 'black',
+          borderWidth: 1,
+          borderColor: 'lightgray',
+          marginBottom: 20,
+          fontSize: 16,
+          padding: 5,
+        }}
+        value={description}
+        onChangeText={text => setDescription(text)}
+        placeholder="Add description"
+        multiline={true} // Enable multiline
+        numberOfLines={4} // Adjust the number of lines as needed
+        onBlur={updateDescriptionInFirestore} // Update description when focus is lost
       />
     </View>
   );
