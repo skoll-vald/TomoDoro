a40ee932eeaefeb0a3fe9c286540650d76a8b236
skoll-vald <skollvald@gmail.com>
2023-11-17 14:42:54 +0300

    Feat: add recursive deletion of child tasks with parent

--- deleteTask.tsx
+++ deleteTask.tsx
@@ -4,41 +4,83 @@ import {Alert} from 'react-native';
 import {fetchTasks} from './fetchTasks';
 import {Task} from './Task';
 
-export const deleteTask = async (
+const deleteTaskRecursive = async (
   taskId: string,
   parentTaskId: string,
   setTasks: React.Dispatch<React.SetStateAction<Task[]>>,
 ) => {
   try {
-    // Show a confirmation popup before deleting the task
-    Alert.alert(
-      'Think slow, decide fast',
-      'Are you really-really pretty-pretty sure you want to delete this task?',
-      [
-        {
-          text: 'Cancel',
-          style: 'cancel',
-        },
-        {
-          text: 'Delete',
-          onPress: async () => {
-            const currentUser = auth().currentUser;
-            if (currentUser) {
-              await firestore()
-                .collection('users')
-                .doc(currentUser.uid)
-                .collection('tasks')
-                .doc(taskId)
-                .delete();
-              fetchTasks(parentTaskId, setTasks); // Fetch the updated Tasks for the user
-            }
-          },
-          style: 'destructive',
-        },
-      ],
-      {cancelable: true},
-    );
+    const currentUser = auth().currentUser;
+    if (currentUser) {
+      // Fetch the task and its children
+      const taskSnapshot = await firestore()
+        .collection('users')
+        .doc(currentUser.uid)
+        .collection('tasks')
+        .doc(taskId)
+        .get();
+
+      const task = taskSnapshot.data() as Task;
+
+      if (task) {
+        // Recursively delete children
+        const deleteChildTasks = async (parentId: string) => {
+          const childrenSnapshot = await firestore()
+            .collection('users')
+            .doc(currentUser.uid)
+            .collection('tasks')
+            .where('parentId', '==', parentId)
+            .get();
+
+          const childTasks = childrenSnapshot.docs.map(doc => doc.id);
+          const deletePromises = childTasks.map(childTaskId =>
+            deleteTaskRecursive(childTaskId, parentTaskId, setTasks),
+          );
+
+          await Promise.all(deletePromises);
+        };
+
+        await deleteChildTasks(taskId);
+
+        // Delete the current task
+        await firestore()
+          .collection('users')
+          .doc(currentUser.uid)
+          .collection('tasks')
+          .doc(taskId)
+          .delete();
+
+        // Fetch the updated tasks
+        fetchTasks(parentTaskId, setTasks);
+      }
+    }
   } catch (error) {
     console.error('Error deleting Task:', error);
   }
+};
+
+export const deleteTask = async (
+  taskId: string,
+  parentTaskId: string,
+  setTasks: React.Dispatch<React.SetStateAction<Task[]>>,
+) => {
+  // Show a confirmation popup before deleting the task
+  Alert.alert(
+    'Think slow, decide fast',
+    'Are you really-really pretty-pretty sure you want to delete this task?',
+    [
+      {
+        text: 'Cancel',
+        style: 'cancel',
+      },
+      {
+        text: 'Delete',
+        onPress: async () => {
+          await deleteTaskRecursive(taskId, parentTaskId, setTasks);
+        },
+        style: 'destructive',
+      },
+    ],
+    {cancelable: true},
+  );
 };
